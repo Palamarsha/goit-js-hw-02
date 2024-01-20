82989c732445285b41f5cee3d354be6db41c0a1d
Palamarsha <149620290+Palamarsha@users.noreply.github.com>
2024-01-20 22:07:57 +0200

    ad task 2

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+function formatMessage(message, maxLength) {
+    
+    if (message.length <= maxLength) {
+        infoMessage = message.slice(0);
+    } else if (message.length > maxLength) {
+        infoMessage = message.slice(0, maxLength) + "...";
+    }
+    // console.log(message.length);
+    // console.log(infoMessage);
+    // console.log(message);
+    return infoMessage;
+    
+}
+formatMessage("Curabitur ligula sapien", 16);
+formatMessage("Curabitur ligula sapien", 23);
+formatMessage("Vestibulum facilisis purus nec", 20);
+formatMessage("Vestibulum facilisis purus nec", 30);
+formatMessage("Nunc sed turpis a felis in nunc fringilla", 15);
+formatMessage("Nunc sed turpis a felis in nunc fringilla", 41);
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
