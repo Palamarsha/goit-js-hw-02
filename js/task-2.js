44c18bc480dcbc0031118a030194c688624da717
Palamarsha <149620290+Palamarsha@users.noreply.github.com>
2024-01-21 20:53:57 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -5,18 +5,8 @@ function formatMessage(message, maxLength) {
     } else if (message.length > maxLength) {
         infoMessage = message.slice(0, maxLength) + "...";
     }
-    // console.log(message.length);
-    // console.log(infoMessage);
-    // console.log(message);
     return infoMessage;
-    
 }
-formatMessage("Curabitur ligula sapien", 16);
-formatMessage("Curabitur ligula sapien", 23);
-formatMessage("Vestibulum facilisis purus nec", 20);
-formatMessage("Vestibulum facilisis purus nec", 30);
-formatMessage("Nunc sed turpis a felis in nunc fringilla", 15);
-formatMessage("Nunc sed turpis a felis in nunc fringilla", 41);
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
